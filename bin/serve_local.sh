2c1fcc325a2543d9531ee3931487754a40c29880
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-05-21 13:09:27 +0900

    Generate LLMs text during site build

--- bin/serve_local.sh
+++ bin/serve_local.sh
@@ -3,4 +3,6 @@
 # 'bundle exec' ensures we're using the correct versions of each gem according to our Gemfile.lock.
 # 'jekyll serve' starts a Jekyll development server.
 # '--watch' option automatically rebuilds the site when files are modified.
+
+php bin/generate_llms_full.php
 bundle exec jekyll serve --watch --trace
